5d94d90358f6d287fe4b8d6d78aded321d003d57
jonathanhawley <jdhawley@crimson.ua.edu>
2019-07-29 19:49:45 -0400

    Added model for todo items

---
+++ Todoey/TodoItem.swift
@@ -0,0 +1,24 @@
+//
+//  TodoItem.swift
+//  Todoey
+//
+//  Created by Jonathan Hawley on 7/29/19.
+//  Copyright © 2019 Jonathan Hawley. All rights reserved.
+//
+
+import Foundation
+
+class TodoItem{
+    var todoText: String
+    var isComplete: Bool
+    
+    init(todoText: String){
+        self.todoText = todoText
+        self.isComplete = false
+    }
+    
+    init(todoText: String, isComplete: Bool){
+        self.todoText = todoText
+        self.isComplete = isComplete
+    }
+}
--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,20 +11,28 @@ import UIKit
 private let dataKey = "TodoItems"
 
 class TodoListViewController: UITableViewController {
-    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = [TodoItem]()
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: dataKey) as? [String]{
+        if let items = defaults.array(forKey: dataKey) as? [TodoItem]{
             itemArray = items
+        }
+        else{
+            itemArray.append(TodoItem(todoText: "Find Mike"))
+            itemArray.append(TodoItem(todoText: "Buy Eggos"))
+            itemArray.append(TodoItem(todoText: "Destroy Demogorgon"))
         }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let todo = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = todo.todoText
+        cell.accessoryType = todo.isComplete ? .checkmark : .none
         
         return cell
     }
@@ -44,8 +52,8 @@ class TodoListViewController: UITableViewController {
         alert.addAction(UIAlertAction(title: "OK", style: .default) { (_) in
             guard let todoText = alert.textFields?[0].text
                 else{ fatalError("Todo text was unavailable") }
-            self.itemArray.append(todoText)
-            self.defaults.set(self.itemArray, forKey: dataKey)
+            self.itemArray.append(TodoItem(todoText: todoText))
+//            self.defaults.set(self.itemArray, forKey: dataKey)
             self.tableView.reloadData()
         })
         
@@ -60,8 +68,9 @@ class TodoListViewController: UITableViewController {
     
     private func toggleComplete(indexPath: IndexPath){
         guard let cell = tableView.cellForRow(at: indexPath) else{ fatalError("IndexPath out of range") }
+        let todo = itemArray[indexPath.row]
         
+        todo.isComplete = !todo.isComplete
         cell.accessoryType = cell.accessoryType == .none ? .checkmark : .none
     }
 }
-
