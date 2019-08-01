fa9a6a584b8a9b4f45cd04fb01a57538c1747542
jonathanhawley <jdhawley@crimson.ua.edu>
2019-08-01 17:00:03 -0400

    Adding TodoItems now works

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,32 +7,35 @@
 //
 
 import UIKit
-import CoreData
+import RealmSwift
 
 class TodoListViewController: UITableViewController {
+    let realm = try! Realm()
+    
+    var todoItems: Results<TodoItem>?
     var parentCategory: Category?
-    var itemArray = [TodoItem]()
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        loadItems(forCategory: parentCategory!.categoryName!)
+        loadItems()
     }
 
     //MARK: TableView Delegate methods
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        let todo = itemArray[indexPath.row]
-        
-        cell.textLabel?.text = todo.todoText
-        cell.accessoryType = todo.isComplete ? .checkmark : .none
+        if let todo = todoItems?[indexPath.row]{
+            cell.textLabel?.text = todo.todoText
+            cell.accessoryType = todo.isComplete ? .checkmark : .none
+        }else{
+            cell.textLabel?.text = "No items added"
+        }
         
         return cell
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+        return todoItems?.count ?? 1
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -48,14 +51,18 @@ class TodoListViewController: UITableViewController {
         alert.addAction(UIAlertAction(title: "OK", style: .default) { (_) in
             guard let todoText = alert.textFields?[0].text
                 else{ fatalError("Todo text was unavailable") }
-//
-//            let item = TodoItem(context: self.context)
-//            item.todoText = todoText
-//            item.isComplete = false
-//            item.parentCategory = self.parentCategory
-//            self.itemArray.append(item)
             
-            self.saveData()
+            if let currentCategory = self.parentCategory{
+                do{
+                    try self.realm.write {
+                        let item = TodoItem()
+                        item.todoText = todoText
+                        currentCategory.todoItems.append(item)
+                    }
+                } catch{
+                    print("Error saving realm: \(error)")
+                }
+            }
             
             self.tableView.reloadData()
         })
@@ -72,39 +79,17 @@ class TodoListViewController: UITableViewController {
     //MARK: Private Functions
     private func toggleComplete(indexPath: IndexPath){
         guard let cell = tableView.cellForRow(at: indexPath) else{ fatalError("IndexPath out of range") }
-        let todo = itemArray[indexPath.row]
-        
-        todo.isComplete = !todo.isComplete
-        
-        saveData()
+
+        if let todo = todoItems?[indexPath.row]{
+            todo.isComplete = !todo.isComplete
+        }
         
         cell.accessoryType = cell.accessoryType == .none ? .checkmark : .none
     }
     
-    private func saveData(){
-        do{
-            try context.save()
-        } catch{
-            print("Error saving context: \(error)")
-        }
+    private func loadItems(){
+        todoItems = parentCategory?.todoItems.sorted(byKeyPath: "todoText", ascending: true)
     }
-    
-//    private func loadItems(forCategory categoryName: String){
-//        let request: NSFetchRequest<TodoItem> = TodoItem.fetchRequest()
-//        request.predicate = NSPredicate(format: "ANY parentCategory.categoryName == %@", categoryName)
-//
-//        loadItems(withRequest: request)
-//    }
-//
-//    private func loadItems(withRequest request: NSFetchRequest<TodoItem> = TodoItem.fetchRequest()){
-//        do{
-//            itemArray = try context.fetch(request)
-//        } catch{
-//            print("Error fetching data from context: \(error)")
-//        }
-//
-//        tableView.reloadData()
-//    }
 }
 
 //extension TodoListViewController: UISearchBarDelegate{
